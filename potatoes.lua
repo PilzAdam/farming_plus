793551e713e8468ad3f804e008e54b1a9c083198
Vanessa Ezekowitz <vanessaezekowitz@gmail.com>
2013-10-09 10:09:40 -0400

    Fix misspellings in potatoes
    (the singular is "potato", not "potatoe")

--- potatoes.lua
+++ potatoes.lua
@@ -1,10 +1,10 @@
-minetest.register_craftitem("farming_plus:potatoe_seed", {
-	description = "Potatoe Seeds",
-	inventory_image = "farming_potatoe_seed.png",
+minetest.register_craftitem("farming_plus:potato_seed", {
+	description = "Potato Seeds",
+	inventory_image = "farming_potato_seed.png",
 	on_place = function(itemstack, placer, pointed_thing)
 		local above = minetest.env:get_node(pointed_thing.above)
 		if above.name == "air" then
-			above.name = "farming_plus:potatoe_1"
+			above.name = "farming_plus:potato_1"
 			minetest.env:set_node(pointed_thing.above, above)
 			itemstack:take_item(1)
 			return itemstack
@@ -12,12 +12,12 @@ minetest.register_craftitem("farming_plus:potatoe_seed", {
 	end
 })
 
-minetest.register_node("farming_plus:potatoe_1", {
+minetest.register_node("farming_plus:potato_1", {
 	paramtype = "light",
 	walkable = false,
 	drawtype = "plantlike",
 	drop = "",
-	tiles = {"farming_potatoe_1.png"},
+	tiles = {"farming_potato_1.png"},
 	selection_box = {
 		type = "fixed",
 		fixed = {
@@ -28,12 +28,12 @@ minetest.register_node("farming_plus:potatoe_1", {
 	sounds = default.node_sound_leaves_defaults(),
 })
 
-minetest.register_node("farming_plus:potatoe_2", {
+minetest.register_node("farming_plus:potato_2", {
 	paramtype = "light",
 	walkable = false,
 	drawtype = "plantlike",
 	drop = "",
-	tiles = {"farming_potatoe_2.png"},
+	tiles = {"farming_potato_2.png"},
 	selection_box = {
 		type = "fixed",
 		fixed = {
@@ -44,29 +44,36 @@ minetest.register_node("farming_plus:potatoe_2", {
 	sounds = default.node_sound_leaves_defaults(),
 })
 
-minetest.register_node("farming_plus:potatoe", {
+minetest.register_node("farming_plus:potato", {
 	paramtype = "light",
 	walkable = false,
 	drawtype = "plantlike",
-	tiles = {"farming_potatoe_3.png"},
+	tiles = {"farming_potato_3.png"},
 	drop = {
 		max_items = 6,
 		items = {
-			{ items = {'farming_plus:potatoe_seed'} },
-			{ items = {'farming_plus:potatoe_seed'}, rarity = 2},
-			{ items = {'farming_plus:potatoe_seed'}, rarity = 5},
-			{ items = {'farming_plus:potatoe_item'} },
-			{ items = {'farming_plus:potatoe_item'}, rarity = 2 },
-			{ items = {'farming_plus:potatoe_item'}, rarity = 5 }
+			{ items = {'farming_plus:potato_seed'} },
+			{ items = {'farming_plus:potato_seed'}, rarity = 2},
+			{ items = {'farming_plus:potato_seed'}, rarity = 5},
+			{ items = {'farming_plus:potato_item'} },
+			{ items = {'farming_plus:potato_item'}, rarity = 2 },
+			{ items = {'farming_plus:potato_item'}, rarity = 5 }
 		}
 	},
 	groups = {snappy=3, flammable=2, not_in_creative_inventory=1,plant=1},
 	sounds = default.node_sound_leaves_defaults(),
 })
 
-minetest.register_craftitem("farming_plus:potatoe_item", {
-	description = "Potatoe",
-	inventory_image = "farming_potatoe.png",
+minetest.register_craftitem("farming_plus:potato_item", {
+	description = "Potato",
+	inventory_image = "farming_potato.png",
 })
 
-farming:add_plant("farming_plus:potatoe", {"farming_plus:potatoe_1", "farming_plus:potatoe_2"}, 50, 20)
+farming:add_plant("farming_plus:potato", {"farming_plus:potato_1", "farming_plus:potato_2"}, 50, 20)
+
+minetest.register_alias("farming_plus:potatoe_item", "farming_plus:potato_item")
+minetest.register_alias("farming_plus:potatoe_seed", "farming_plus:potato_seed")
+minetest.register_alias("farming_plus:potatoe", "farming_plus:potato")
+minetest.register_alias("farming_plus:potatoe_1", "farming_plus:potato_1")
+minetest.register_alias("farming_plus:potatoe_2", "farming_plus:potato_2")
+
