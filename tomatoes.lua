cc35745e11fe1659d67886dd77fec56f2257cc97
Xanthin <Xanthin@users.noreply.github.com>
2014-03-08 03:33:36 +0100

    Modified tomatoes.lua for translation

--- tomatoes.lua
+++ tomatoes.lua
@@ -7,43 +7,92 @@ else
   S = function ( s ) return s end
 end
 
-minetest.register_node(":farming:weed", {
-	description = S("Weed"),
-	paramtype = "light",
-	sunlight_propagates = true,
-	walkable = false,
-	drawtype = "plantlike",
-	tiles = {"farming_weed.png"},
-	inventory_image = "farming_weed.png",
-	selection_box = {
-		type = "fixed",
-		fixed = {
-			{-0.5, -0.5, -0.5, 0.5, -0.5+4/16, 0.5}
-		},
-	},
-	groups = {snappy=3, flammable=2,plant=1},
-	sounds = default.node_sound_leaves_defaults()
-})
-
-minetest.register_abm({
-	nodenames = {"farming:soil_wet", "farming:soil"},
-	interval = 50,
-	chance = 10,
-	action = function(pos, node)
-		if minetest.env:find_node_near(pos, 4, {"farming:scarecrow", "farming:scarecrow_light"}) ~= nil then
-			return
-		end
-		pos.y = pos.y+1
-		if minetest.env:get_node(pos).name == "air" then
-			node.name = "farming:weed"
-			minetest.env:set_node(pos, node)
+minetest.register_craftitem("farming_plus:tomato_seed", {
+	description = S("Tomato Seeds"),
+	inventory_image = "farming_tomato_seed.png",
+	on_place = function(itemstack, placer, pointed_thing)
+		local above = minetest.env:get_node(pointed_thing.above)
+		if above.name == "air" then
+			above.name = "farming_plus:tomato_1"
+			minetest.env:set_node(pointed_thing.above, above)
+			itemstack:take_item(1)
+			return itemstack
 		end
 	end
 })
 
--- ========= FUEL =========
-minetest.register_craft({
-	type = "fuel",
-	recipe = "farming:weed",
-	burntime = 1
+minetest.register_node("farming_plus:tomato_1", {
+	paramtype = "light",
+	walkable = false,
+	drawtype = "plantlike",
+	drop = "",
+	tiles = {"farming_tomato_1.png"},
+	selection_box = {
+		type = "fixed",
+		fixed = {
+			{-0.5, -0.5, -0.5, 0.5, -0.5+5/16, 0.5}
+		},
+	},
+	groups = {snappy=3, flammable=2, not_in_creative_inventory=1,plant=1},
+	sounds = default.node_sound_leaves_defaults(),
 })
+
+minetest.register_node("farming_plus:tomato_2", {
+	paramtype = "light",
+	walkable = false,
+	drawtype = "plantlike",
+	drop = "",
+	tiles = {"farming_tomato_2.png"},
+	selection_box = {
+		type = "fixed",
+		fixed = {
+			{-0.5, -0.5, -0.5, 0.5, -0.5+8/16, 0.5}
+		},
+	},
+	groups = {snappy=3, flammable=2, not_in_creative_inventory=1,plant=1},
+	sounds = default.node_sound_leaves_defaults(),
+})
+
+minetest.register_node("farming_plus:tomato_3", {
+	paramtype = "light",
+	walkable = false,
+	drawtype = "plantlike",
+	drop = "",
+	tiles = {"farming_tomato_3.png"},
+	selection_box = {
+		type = "fixed",
+		fixed = {
+			{-0.5, -0.5, -0.5, 0.5, -0.5+13/16, 0.5}
+		},
+	},
+	groups = {snappy=3, flammable=2, not_in_creative_inventory=1,plant=1},
+	sounds = default.node_sound_leaves_defaults(),
+})
+
+minetest.register_node("farming_plus:tomato", {
+	paramtype = "light",
+	walkable = false,
+	drawtype = "plantlike",
+	tiles = {"farming_tomato_4.png"},
+	drop = {
+		max_items = 6,
+		items = {
+			{ items = {'farming_plus:tomato_seed'} },
+			{ items = {'farming_plus:tomato_seed'}, rarity = 2},
+			{ items = {'farming_plus:tomato_seed'}, rarity = 5},
+			{ items = {'farming_plus:tomato_item'} },
+			{ items = {'farming_plus:tomato_item'}, rarity = 2 },
+			{ items = {'farming_plus:tomato_item'}, rarity = 5 }
+		}
+	},
+	groups = {snappy=3, flammable=2, not_in_creative_inventory=1,plant=1},
+	sounds = default.node_sound_leaves_defaults(),
+})
+
+minetest.register_craftitem("farming_plus:tomato_item", {
+	description = S("Tomato"),
+	inventory_image = "farming_tomato.png",
+	on_use = minetest.item_eat(4),
+})
+
+farming:add_plant("farming_plus:tomato", {"farming_plus:tomato_1", "farming_plus:tomato_2", "farming_plus:tomato_3"}, 50, 20)
